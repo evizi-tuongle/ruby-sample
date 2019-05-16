17478cd269456ac4c7c1324929a67ce97bc67429
Tuong Le <lexuantuongbk@gmail.com>
2019-05-16 16:06:24 +0700

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
 	attr_accessor :remember_token
 	before_save :downcase_email
-	before_create :create_activation_digest	
 
 	validates :name, presence: true, length: { maximum: 50 }
 	VALIDATE_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -43,9 +42,5 @@ class User < ApplicationRecord
       self.email = email.downcase
     end
 
-    # Creates and assigns the activation token and digest.
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+  
 end
